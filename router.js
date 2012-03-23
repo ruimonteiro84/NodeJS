41d863cdf8aecb043c2b98501a49dca653e83e4e
Daniel Gomes <daniel.gomes@reset.pt>
2012-03-23 01:55:20 +0000

    fixed the issue

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    console.log("visited: " + pathname);
+    return pathname;
   } else {
     console.log("No request handler found for " + pathname);
-	return "404 Request Not found";
+    return "404 Request Not found";
   }
 }
 exports.route = route;
